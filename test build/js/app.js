49fea5790b2f28607643f81e75e3529a25764f3d
Jeremy Katras <jkatras@fullsail.edu>
2016-01-19 17:38:23 -0500

    init file structure

---
+++ test build/js/app.js
@@ -0,0 +1,12 @@
+var body = document.getElementById("body"); 
+var canvas, hex;
+
+var draw = function(){
+	var canvas = document.getElementById("mainCanvas");
+        if (canvas.getContext){
+          var ctx = canvas.getContext('2d');
+  
+        }
+
+}
+body.on('load', draw());
