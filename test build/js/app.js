d51e8988cee07faaf928578f476357809324f362
Jeremy Katras <jkatras@fullsail.edu>
2016-02-08 17:40:43 -0500

    sync

--- test build/js/app.js
+++ test build/js/app.js
@@ -1,14 +1,14 @@
 // var body = document.getElementById("body"); 
-var canvas, hex;
+var canvas = document.getElementById("mainCanvas"); 
+var ctx = canvas.getContext('2d');
 
-var draw = function(){
-	var canvas = document.getElementById("mainCanvas");
-        if (canvas.getContext){
-          var ctx = canvas.getContext('2d');
-        }else{
-        	body.insert("");
-        }
+
+var draw = function(){	
 
 }
 
+var newSprite = function(sprite, width, height){
+	canvas.addChild
+}
 document.body.on('load', draw());
+canvas.appendChild()
